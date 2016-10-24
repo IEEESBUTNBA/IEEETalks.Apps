9ebde5473bb8ef86e3c7ff7b7e352f18063f37f7
ignacioliveros <ignacioliveros@gmail.com>
2016-10-23 23:41:19 -0300

    add aboutUs

--- Angular2-IEEE-App/app/aboutUs/aboutUs.module.ts
+++ Angular2-IEEE-App/app/aboutUs/aboutUs.module.ts
@@ -3,10 +3,12 @@ import { NgModule }      from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {AboutUsRoutingModule} from './aboutUs-routes/aboutUs-routing.module';
 import {AboutUs} from './aboutUs/aboutUs.component';
+import {AboutService} from './aboutUs-service/aboutUs.service';
 
 @NgModule({
     imports:[SharedModule,AboutUsRoutingModule],
-    declarations:[AboutUs]
+    declarations:[AboutUs],
+    providers:[AboutService]
 })
 export class AboutUsModule{
 
